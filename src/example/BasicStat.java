126b3b2cf2f25ec61678ee184fabbccdda8de09b
Martin Quinson <martin.quinson@loria.fr>
2016-11-08 22:31:10 +0100

    count the amount of attempts per exercise per student

--- src/example/BasicStat.java
+++ src/example/BasicStat.java
@@ -23,28 +23,50 @@ public class BasicStat {
 
 
 		RepoIterator ite = new RepoIterator();
+		
 		//option for the iterator
-
 		ite.setCollectCode(true);
 		ite.setCollectError(true);
+		ite.quiet();
 
 		ite.addCommitType(Event.Executed);
 
+		/*
 		Calendar dateMin = Calendar.getInstance();
 		dateMin.set(Calendar.YEAR, 2015);dateMin.set(Calendar.MONTH, 8);dateMin.set(Calendar.DATE, 1);
 		ite.setDateMin(dateMin);
+		*/
 
-		int count = 0;
+		int activeBranch = 0;
 		int nbreExec = 0;
-		while(ite.hasNext()){
+		while(ite.hasNext() && activeBranch < 5){
 			Student student = ite.next();
-			if(student != null){
+			if (student != null) {
+				System.out.println(activeBranch+": "+student.getBranchName());
+				for (String exoName : student.getTriedExoName()) {
+					int failedAttempts = 0;
+					int compilErrors = 0;
+					int succeed = 0;
+					for (Event evt: student.getEventsByExo(exoName)) {						
+						if (evt.getResultCompil().equals("compilation error"))
+							compilErrors++;
+						else if (evt.getResultCompil().equals("failed"))
+							failedAttempts++;
+						else if (evt.getResultCompil().equals("success"))
+							succeed++;
+						else {
+							System.out.println(exoName+": "+evt);
+							System.exit(1);
+						}
+					}
+					System.out.println(exoName+": "+ compilErrors+","+failedAttempts+","+succeed);
+				}
 				nbreExec += student.getEvents().size();
-				count ++;
+				activeBranch ++;
 			}
 		}
 
-		System.out.println("\n branche active : " + count + " exec : " + nbreExec);
+		System.out.println("\n branche active : " + activeBranch + " exec : " + nbreExec);
 
 	}
 }
